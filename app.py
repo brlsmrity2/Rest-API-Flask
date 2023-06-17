0ab9cb31567464323b7bc668a4dadea5406a6c66
Smrity Baral <brlsmrity8@gmail.com>
2023-06-17 19:39:29 +0545

    adde

--- app.py
+++ app.py
@@ -1,48 +1,55 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, marshal_with, fields
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-app = Flask(__name__)
-api = Api(app)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
 
 fakeDatabase = {
-    1:{'name':'Clean car'},
-    2:{'name':'Write blog'},
-    3:{'name':'Start stream'},
+    1: {'name': 'Clean car'},
+    2: {'name': 'Write blog'},
+    3: {'name': 'Start stream'},
+}
+
+taskFields = {
+    'id': fields.Integer,
+    'name': fields.String
 }
 
 class Items(Resource):
+    @marshal_with(taskFields)
     def get(self):
-        return fakeDatabase
+       task = Task.query.all()
+       return task
     
+    @marshal_with(taskFields)
     def post(self):
         data = request.json
-        itemId = len(fakeDatabase.keys())+1
-        fakeDatabase[itemId]={"name": data["name"]}
-        return fakeDatabase
+        task = Task(name=data['name'])
+        db.session.add(task)
+        db.session.commit()
+        return Task.query.all()
 
 class Item(Resource):
+    @marshal_with(taskFields)
     def get(self, pk):
-        return fakeDatabase[pk]   
-
-
-    def put(self, pk):
-        data = request.json
-        fakeDatabase[pk]['name'] = data['name']
-        return fakeDatabase
-    
-    def delete(self, pk):
-        del fakeDatabase[pk]
-        return fakeDatabase
+        task = Task.query.filter_by(id=pk).first()
+        return task  
 
 api.add_resource(Items, '/')
 api.add_resource(Item, '/<int:pk>')
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
