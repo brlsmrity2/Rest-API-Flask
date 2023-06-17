fd82b62d9987cc1a40914fc6354a41edfbc5633b
Smrity Baral <brlsmrity8@gmail.com>
2023-06-17 18:38:57 +0545

    added basic CRUD for static database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
 
 
 app = Flask(__name__)
@@ -16,9 +21,25 @@ class Items(Resource):
     def get(self):
         return fakeDatabase
     
+    def post(self):
+        data = request.json
+        itemId = len(fakeDatabase.keys())+1
+        fakeDatabase[itemId]={"name": data["name"]}
+        return fakeDatabase
+
 class Item(Resource):
     def get(self, pk):
-        return fakeDatabase[pk]    
+        return fakeDatabase[pk]   
+
+
+    def put(self, pk):
+        data = request.json
+        fakeDatabase[pk]['name'] = data['name']
+        return fakeDatabase
+    
+    def delete(self, pk):
+        del fakeDatabase[pk]
+        return fakeDatabase
 
 api.add_resource(Items, '/')
 api.add_resource(Item, '/<int:pk>')
